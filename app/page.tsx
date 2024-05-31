192b5225d5ea5c5ff4c96375a3c41aaf20a8ecf9
lim0n3 <simonestaffa96@gmail.com>
2024-05-31 16:35:06 +0100

    push redirect

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import Link from "next/link";
 import { appURL } from "./utils";
 import { DebugLink } from "./components/DebugLink";
+import { RedirectToHome } from "./components/Redirect";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
@@ -17,13 +18,5 @@ export async function generateMetadata(): Promise<Metadata> {
 // This is a react server component only
 export default async function Home() {
   // then, when done, return next frame
-  return (
-    <div className="p-4">
-      frames.js starter kit. The Template Frame is on this page, it&apos;s in
-      the html meta tags (inspect source). <DebugLink /> or see{" "}
-      <Link href="/examples" className="underline">
-        other examples
-      </Link>
-    </div>
-  );
+  return <RedirectToHome />;
 }
