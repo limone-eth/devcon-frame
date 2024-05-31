09d614c3241a48f35b3f49aa91985d1b7fcebaca
lim0n3 <simonestaffa96@gmail.com>
2024-05-31 16:27:01 +0100

    Devcon

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -4,35 +4,30 @@ import { frames } from "./frames";
 import { appURL } from "../utils";
 
 const frameHandler = frames(async (ctx) => {
-  const counter = ctx.message
-    ? ctx.searchParams.op === "+"
-      ? ctx.state.counter + 1
-      : ctx.state.counter - 1
-    : ctx.state.counter;
-
   return {
-    image: (
-      <div tw="flex flex-col">
-        <div tw="flex">frames.js starter</div>
-        {ctx.message?.inputText && (
-          <div tw="flex">{`Input: ${ctx.message.inputText}`}</div>
-        )}
-        <div tw="flex">Counter {counter}</div>
-      </div>
-    ),
-    textInput: "Say something",
+    image: `${appURL()}/landing.gif`,
     buttons: [
-      <Button action="post" target={{ pathname: "/", query: { op: "+" } }}>
-        Increment
+      <Button action="link" target="https://devcon.org">
+        Website
       </Button>,
-      <Button action="post" target={{ pathname: "/", query: { op: "-" } }}>
-        Decrement
+      <Button action="link" target="https://devcon.org/en/road-to-devcon/">
+        Road to DEVCON
       </Button>,
-      <Button action="link" target={appURL()}>
-        External
+      <Button
+        action="link"
+        target="https://esp.ethereum.foundation/devcon-grants"
+      >
+        Grants
+      </Button>,
+      <Button
+        action="link"
+        target={
+          "https://www.google.com/calendar/render?action=TEMPLATE&ctz=Asia%2FBangkok&text=Devcon+7&details=Devcon+-+The+Ethereum+Developer+Conference&location=QNSCC+%E2%80%94+Queen+Sirikit+National+Convention+Center&dates=20241112%2F20241116"
+        }
+      >
+        Add to Calendar
       </Button>,
     ],
-    state: { counter: counter },
   };
 });
 
